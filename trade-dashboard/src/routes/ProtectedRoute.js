a412eee528c5a6cc7d9936cc59dc84a72203b72a
Isaac-Tellezs <andreescalante34@gmail.com>
2024-11-12 18:28:03 -0600

    add spinner

--- trade-dashboard/src/routes/ProtectedRoute.js
+++ trade-dashboard/src/routes/ProtectedRoute.js
@@ -1,13 +1,25 @@
 // ProtectedRoute.js
 import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate } from 'react-router-dom';
+import { Box, CircularProgress } from '@mui/material';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
 
   
-  if (isLoading) return <p>Cargando...</p>;
-
+ 
+ if (isLoading) {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
 
   if (!isAuthenticated) {
     loginWithRedirect(); 
